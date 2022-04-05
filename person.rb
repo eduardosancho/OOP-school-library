d1ae42f56a893befd52e154cd0f7d1f9fcfbbe52
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-05 14:53:03 -0600

    modify person's setters and getters to attr...

--- person.rb
+++ person.rb
@@ -1,24 +1,12 @@
 class Person
+
+  attr_reader :id
+  attr_accessor :name, :age, :parent_permission
+
   def initialize(name = "Unkown", age, parent_permission = true)
     @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
-  end
-
-  def id
-    @id
-  end
-
-  def name
-    @name
-  end
-
-  def age
-    @age
-  end
-
-  def name
-    @name
   end
 end
