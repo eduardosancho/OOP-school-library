7f6afb73119dab3f64edd94a1bc94d5ab1f73c3f
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-05 14:50:52 -0600

    create person class with initialize

---
+++ person.rb
@@ -0,0 +1,24 @@
+class Person
+  def initialize(name = "Unkown", age, parent_permission = true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  def name
+    @name
+  end
+end
