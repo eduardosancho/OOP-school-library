44e738ed4529a5be1b3a7a297bc2eca47e83fb99
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-07 21:26:32 -0600

    add add_student method for classroom

--- classroom.rb
+++ classroom.rb
@@ -5,4 +5,9 @@ class Classroom
     @label = label
     @students = []
   end
+
+  def add_student(student)
+    @students.push(student) unless @students.include?(student)
+    student.classroom = self
+  end
 end
