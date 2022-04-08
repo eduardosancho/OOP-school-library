f86bf8f8d1b22599d55e31780905da2532edd1fa
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-07 21:25:01 -0600

    change name of method to add_classroom

--- student.rb
+++ student.rb
@@ -11,7 +11,7 @@ class Student < Person
     "¯\(ツ)/¯"
   end
 
-  def classroom=(classroom)
+  def add_classroom(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
