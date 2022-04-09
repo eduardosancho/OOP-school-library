d28ab249a6b187d06a3aca688dce634deac7b926
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-08 21:56:07 -0600

    create app.rb and main.rb

---
+++ app.rb
@@ -0,0 +1,26 @@
+require_relative 'book'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'rental'
+
+class App
+  attr_accessor :books, :people, :rentals
+
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+  end
+
+  def run
+    puts "Welcome to School Library App!"
+    puts "\nPlease choose an option by entering a number:"
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals'
+    puts '7 - Exit'
+  end
+end
