499dad2ba2ff54a7224daf848ed845d6c150a61e
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-08 22:04:43 -0600

    add breaking condition to the run loop

--- app.rb
+++ app.rb
@@ -13,14 +13,19 @@ class App
   end
 
   def run
-    puts "Welcome to School Library App!"
-    puts "\nPlease choose an option by entering a number:"
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals'
-    puts '7 - Exit'
+    loop do
+      puts "Welcome to School Library App!"
+      puts "\nPlease choose an option by entering a number:"
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals'
+      puts '7 - Exit'
+
+      selection = gets.chomp.to_i
+      break if selection == 7
+    end
   end
 end
